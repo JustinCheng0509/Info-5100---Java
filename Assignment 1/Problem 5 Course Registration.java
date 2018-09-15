fe4555849c1b27a9ffaabfddb972ff63e5edc64b
JustinCheng0509 <Yusi.cheng.0509@gmail.com>
2018-09-14 20:51:59 -0700

    Edit

--- Assignment 1/Problem 5 Course Registration.java
+++ Assignment 1/Problem 5 Course Registration.java
@@ -5,3 +5,44 @@ Yusi Cheng
 	Assignment 1
 	@author Yusi Cheng
 */
+
+Internet
+	Data:
+	Behavior: isconnect
+	
+CourseList
+	Data: Courses, 
+	Behavior: ShowCourseList, check;
+	
+Course:
+	Data: CourseName, CourseNumber, Instructor, Hour, Credit
+	Behavior: isAvailable
+	
+Student
+	Data: Name, StudentID
+	Behavior: WebAdd, WebDrop, Login
+	
+	
+	
+Internet internet;
+CourseList NEU;
+Course Info5100;
+Student john;
+
+while internet.isConnected(){
+	if john.Login() = true
+		NEU.ShowCourseList();
+		john.search(Info5100);
+		if Info5100 = NEU(course){
+			if Info5100.isAvailable(){
+				john.WebAdd()
+			}
+			else return Info5100 + "Not Available")
+		}
+		else return Info5100 + "does not exist";
+	}
+	else{ 
+		student.Login();
+	}
+}
+		
