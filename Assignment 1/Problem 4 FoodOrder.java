87dc6cc5f56b1026d14387c911df746038777213
JustinCheng0509 <Yusi.cheng.0509@gmail.com>
2018-09-14 20:41:18 -0700

    Edit

--- Assignment 1/Problem 4 FoodOrder.java
+++ Assignment 1/Problem 4 FoodOrder.java
@@ -5,3 +5,57 @@ Yusi Cheng
 	Assignment 1
 	@author Yusi Cheng
 */
+
+Restaurant 
+	Data: Hours, Location
+	Behavior: isFull, isOpen
+Menu
+	Data: Appitizer, Pasta, Beef, Poultry, Seafood,: Collection of food typr
+	Behaviors
+
+Customer
+	Data: TableNumber, NumberOfCustomer
+	Behavior: Pay, Add, Order, Cancel,
+	
+Waiter
+	Data: Name, StaffID
+	Behaviour: TakeOrder, ServeDrinks, Serve Food, ClosingCheck
+
+Chef
+	Data: Name, StaffID
+	Behaviour: CookOrder, CheckOrder
+	
+
+Food
+	Data: Name, Type, Price
+	
+	
+Customer customer
+Menu menu
+Waiter waiter
+Chef chef
+
+if Restaurant.isOpen = true{
+	while Restaurant.isFull() = false{
+		customer.enter -> restaurant
+		customer.reviewMenu();
+		customer.Order(lambstew);
+		while Order.isAvailable(){
+			waiter.TakeOrder();
+			waiter.ServeDrinks(Water);
+			chef.cookOrder(lambstew);
+			waiter.serve(lambstew);
+		}
+		else {
+			customer.Order();
+		}
+		customer.pay();
+	}
+	else {
+		return Restaurant.isFull()
+		customer.wait();
+	}
+}
+else {
+	customer.changeRestaurant();
+}
