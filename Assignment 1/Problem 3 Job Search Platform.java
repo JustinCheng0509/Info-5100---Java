04e5358b61ebe1bdb2ca088b7caf6f1c2dd91b1f
JustinCheng0509 <Yusi.cheng.0509@gmail.com>
2018-09-14 20:24:39 -0700

    Edit

--- Assignment 1/Problem 3 Job Search Platform.java
+++ Assignment 1/Problem 3 Job Search Platform.java
@@ -5,3 +5,53 @@ Yusi Cheng
 	Assignment 1
 	@author Yusi Cheng
 */
+
+CompanyRecruit
+	Data: CompanyName, RecruiterName, RecruiterEmail
+	Behavior: AddPosition, DeletePosition, EditPosition, PostPosition
+	
+Platform
+	Data: JobInformation(Collection of jobs)
+	Behavior: SearchJob, CustomLogin, CompanyLogin, Filter
+	
+Custom:
+	Data: Name, PhoneNumber, Resume, Email
+	Behavior: PostResume, Apply
+	
+Internet Internet;
+Platform Indeed;
+CompanyRecruit company;
+Custom Custom;
+
+if Internet.isAvailable
+	if (Indeed.companyLogin = true){
+		Indeed.searchCompany -> company.displayinfo
+		if company.JobAvailable(){
+			
+			company.PostPosition();
+		else
+			company.AddPosition();
+		else
+		return NoJobAvailable();
+		}
+	}
+	if （Indeed.CustomLogin = true){
+		while custom.completeProfile = false {
+			custom.AskcompleteProfile();
+			custom.PostResume();
+			break;
+		}
+		Indeed.Filter -> jobs, salary, desired field;(Job collections)
+		custom.searchJobTitle();
+		if custom.HasDesiredJob{
+			custom.Apply();
+		}
+		else searchJobTitle();
+		("No Job Title Available");
+		else{
+			custom.completeProfile();
+		}
+	}
+else {
+	Internet.reconnect();
+}
